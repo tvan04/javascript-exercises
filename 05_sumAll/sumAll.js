54500821c67576cad12229ec8029977bde5061c3
tvan04 <vantristan04@gmail.com>
2022-07-13 15:19:43 -0400

    Provide functionality for sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (Number.isInteger(a) != true || Number.isInteger(b) != true) return ("ERROR");
+    if (a < 0 || b < 0) return ("ERROR");
+    if (a > b) {
+        var c = a;
+        a = b;
+        b = c;
+        
+    }
+    var sum = 0;
+    for (let i = a; i < b + 1; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
