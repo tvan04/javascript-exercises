a42e7c895e0605d43e1bc6aec0be5a36b7606227
tvan04 <vantristan04@gmail.com>
2022-07-13 15:41:27 -0400

    Provide functionality for conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let x = (f-32)*5/9;
+  return Number(x.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let x = c*(9/5)+32;
+  return Number(x.toFixed(1))
 };
 
 // Do not edit below this line
