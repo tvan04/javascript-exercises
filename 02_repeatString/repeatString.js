e14c0ddb20e6f099cf4273cef209dc1b0c9ed2b3
tvan04 <vantristan04@gmail.com>
2022-07-12 21:55:11 -0400

    provide functionality for the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let fullstring = "";
+    for (let i = 0; i < num; i++) {
+        fullstring += string;
+    }
+   return fullstring;
 };
 
 // Do not edit below this line
